ebaa8183d878048906b6afdef182002db6d9c237
Shantanu Gupta <shantanu.smg@gmail.com>
2020-03-12 22:50:02 -0700

    test upload to s3

---
+++ README.txt
@@ -0,0 +1,8 @@
+# pi-eye-py
+Motion detection and object identification with DarkNet on Raspberry Pi setup
+
+
+1. Run below commands
+	sudo apt-get install python3-pip
+	sudo apt-get install python3-tk
+	pip3 install -r requirements.txt
--- upload.py
+++ upload.py
@@ -1,1 +1,8 @@
-#
+import boto3
+
+# Let's use Amazon S3
+s3 = boto3.resource('s3')
+for bucket in s3.buckets.all():
+    print(bucket.name)
+data = open('test_upload.txt', 'rb')
+s3.Bucket('content-bucket-546').put_object(Key='test_upload.txt', Body=data)
