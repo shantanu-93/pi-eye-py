f379237e6d2a72760c618e54768354ddf143fad7
Shantanu Gupta <shantanu.smg@gmail.com>
2020-03-14 16:29:31 -0700

    upload functions

---
+++ global_constants.py
@@ -0,0 +1,5 @@
+class GlobalConstants:
+
+    def __init__(self):
+        self.VIDEO_BUCKET = 'content-bucket-546'
+        self.RESULTS_BUCKET = 'results-bucket-546'
--- upload.py
+++ upload.py
@@ -1,8 +1,18 @@
 import boto3
+from global_constants import GlobalConstants
+"""
+Upload video file objects to S3
+"""
+def upload_videos(video_files):
+    s3 = boto3.resource('s3')
+    content_bucket = s3.Bucket(GlobalConstants.VIDEO_BUCKET)
+    for video_file in video_files:
+        data = open(video_file, 'rb')
+        content_bucket.put_object(Key=video_file, Body=data)
 
-# Let's use Amazon S3
-s3 = boto3.resource('s3')
-for bucket in s3.buckets.all():
-    print(bucket.name)
-data = open('test_upload.txt', 'rb')
-s3.Bucket('content-bucket-546').put_object(Key='test_upload.txt', Body=data)
+def upload_results(result_files):
+    s3 = boto3.resource('s3')
+    results_bucket = s3.Bucket(GlobalConstants.RESULTS_BUCKET)
+    for result_file in result_files:
+        data = open(result_file, 'rb')
+        results_bucket.put_object(Key=result_file, Body=data)
