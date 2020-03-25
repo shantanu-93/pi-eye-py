03615b641a7e7e60a74fd68dd673ffebcc68558c
Shantanu Gupta <shantanu.smg@gmail.com>
2020-03-25 16:23:58 -0700

    parse and upload to s3

--- analyze_pi.py
+++ analyze_pi.py
@@ -2,9 +2,12 @@
 import os
 import subprocess
 import glob
-import queue_util as queue_util
+import queue_util
 from global_constants import GlobalConstants
+import sys
 import psutil
+import s3_util
+import parse
 
 const = GlobalConstants()
 analysis_dir = "/home/pi/pi-eye-py/pi_videos/"
@@ -26,11 +29,16 @@ if __name__ == '__main__':
                     print("Video File: ",latest_subdir)
                     result_file = os.path.join(result_dir,str(os.path.basename(latest_subdir)[:-5] + '_result.txt'))
                     print("Result File: ",result_file)
+                    output_file = result_file.replace('_result','_output')
                     command = "./darknet detector demo cfg/coco.data cfg/yolov3-tiny.cfg yolov3-tiny.weights {0}".format(latest_subdir)
                     proc = subprocess.Popen([command], stdout=subprocess.PIPE, shell=True)
                     (out, err) = proc.communicate()
                     with open(result_file, 'w') as fout:
                         fout.write(out)
+                    with open(output_file, 'w') as fout:
+                        fout.write(parse.parse_result(output_file))
+                    if output_file is not None:
+                        s3_util.upload_results([output_file])
                     os.system('mv {0} {1}'.format(latest_subdir,processed_dir))
                     # TODO: comment above uncomment below
                     # os.system('rm -rf %s' %latest_subdir)
