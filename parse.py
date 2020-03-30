bd282afe93a46d5a8e1aa4392e39280da3f57f41
michael <michaellin100.0@gmail.com>
2020-03-29 22:09:23 -0700

    parse.py

--- parse.py
+++ parse.py
@@ -2,7 +2,7 @@ from re import search
 # from os.path import isfile
 
 # out is string
-def parse_result(out): 
+def parse_result(out):
     objs = []
     # if isfile(file):
     #     with open(file) as f:
@@ -15,16 +15,4 @@ def parse_result(out):
     return ','.join(str(s) for s in objs)
 
 if __name__ == '__main__':
-<<<<<<< HEAD
-    a = 'demo.txt'
-    r = parse_result(a)
-    print("r:" + r)
-    # r = parse_result('/home/shantanu/pi-eye-py/pi_results/2020-03-25_04.37.54_result.txt')
-    # print(r)
-    # r = parse_result('/home/shantanu/pi-eye-py/pi_results/2020-03-25_04.38.51_result.txt')
-    # print(r)
-    # r = parse_result('/home/shantanu/pi-eye-py/pi_result/2020-03-25_04.38.51_result.txt')
-    # print(r)
-=======
     pass
->>>>>>> 4c1210a0a5dcc996f5ed445e84358725138db011
